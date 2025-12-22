d8a3991323c151b5abd519459fbb352273ae9ed5
leonelcp-dev <leonelcp@gmail.com>
2025-12-22 07:47:14 -0300

    Projeto Leitos SIRESP
    Gerando projeto

---
+++ siresp-leitos/src/main/java/extracao_dados/siresp/App.java
@@ -0,0 +1,13 @@
+package extracao_dados.siresp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
